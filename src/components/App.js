17a0769fc70e3fe192e6b28027d283c428c019ea
tkyg <80114433+tkyg@users.noreply.github.com>
2022-07-26 15:42:04 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React from "react";
-import SushiContainer from "./SushiContainer";
+import React, { useEffect, useState } from "react";
+import SushiContainer from "./SushiContainer"
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushis, setSushis] = useState([])
+  const [beltPosition, setBeltPosition] = useState(0);
+  const [money, setMoney] = useState(100);
+  const DISPLAY_COUNT = 4;
+ 
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then((data) => setSushis(data))
+  }, [])
+
+  function setEaten(piece){
+    const remainingMoney = money - piece.price
+
+    if(!piece.eaten && remainingMoney >= 0) {
+      setMoney(remainingMoney);
+
+      setSushis(
+        sushis.map((sushi) => 
+          sushi.id === piece.id ? {...sushi, eaten: true} : sushi
+        )
+      )
+    }
+  }
+
+  function advanceBelt() {
+    setBeltPosition((beltPosition + DISPLAY_COUNT) % sushis.length)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis.slice(beltPosition, beltPosition + DISPLAY_COUNT)} handleMoreClick={advanceBelt}
+      handleEatSushi={setEaten}
+      />
+      <Table money={money} plates={ sushis.filter(sushi => sushi.eaten) }/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, handleMoreClick, handleEatSushi }) {
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.map((sushi) => (
+        <Sushi key={sushi.id} sushi={sushi} handleEatSushi={handleEatSushi}/>
+      ))}
+      <MoreButton handleMoreClick={handleMoreClick}/>
     </div>
   );
 }
